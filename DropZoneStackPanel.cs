a66c56acfdad9707940c5933c93445dd64e6f072
adam-currie <tfxflrstx@gmail.com>
2020-04-24 16:48:11 -0400

    removed debug code

--- DropZoneStackPanel.cs
+++ DropZoneStackPanel.cs
@@ -1,17 +1,11 @@
 ﻿using Avalonia.Controls;
 using System.Diagnostics;
 using Avalonia.Media;
-using Avalonia.VisualTree;
-using System;
-using System.Collections.Specialized;
 
 namespace SnapUi {
     public class DropZoneStackPanel : StackPanel, IDropZone {
 
-        protected override void ChildrenChanged(object sender, NotifyCollectionChangedEventArgs e) {
-            base.ChildrenChanged(sender, e);
-            IsVisible = Children.Count > 0;//todo: placeholder control
-        }
+        //todo: check out Children.Validate
 
         static DropZoneStackPanel() {
             //making this hit-testable by default
@@ -29,7 +23,7 @@ namespace SnapUi {
         public bool CanAddDraggable(IDraggable draggable) 
             => true;
 
-        public bool RemoveDraggable(IDraggable draggable) 
+        public bool RemoveDraggable(IDraggable draggable)
             => Children.Remove(draggable);
 
         public bool RemovePreview(IPreviewOfDraggable preview)
